6f5539b550e1489f25bbc74495ef141d14b559b4
Barbara Viana <vianabsouza@gmail.com>
2024-10-01 13:50:59 -0300

    feat(frontend): initial structure with nextjs

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Montserrat } from 'next/font/google'
+
+const font = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Gam3r.store",
+  description: "A versão mais básica do Gamr.store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={font.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
