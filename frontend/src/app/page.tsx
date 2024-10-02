9a3b8366fa05ef6494fb5d44e380b2a8964e99cd
Barbara Viana <vianabsouza@gmail.com>
2024-10-02 01:08:37 -0300

    feat: create ProdutoItem component and implement it on the homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,12 @@
+import { produtos } from "@/core";
+import ProdutoItem from "./components/produto/ProdutoItem";
+
 export default function Home() {
   return (
-    <div>Aplicação</div>
+    <div className="grid grid-cols-4 gap-5 container">
+      {produtos.map((produto) => (
+        <ProdutoItem key={produto.id} produto={produto}/>
+      ))}
+    </div>
   );
 }
