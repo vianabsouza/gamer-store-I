f6b14f05c024e8c5cc59281d895e7eb3367b5b1c
Barbara Viana <vianabsouza@gmail.com>
2024-10-04 16:14:47 -0300

    feat: implement method to show products

--- backend/src/produto/produto.controller.ts
+++ backend/src/produto/produto.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { Produto, produtos } from 'src/core';
 
-@Controller('produto')
-export class ProdutoController {}
+@Controller('produtos')
+export class ProdutoController {
+  @Get()
+  obterProduto(): Produto[] {
+    return produtos.map((produto) => ({
+      ...produto,
+      especificacoes: {destaque: produto.especificacoes.destaque}
+    }))
+  }
+}
