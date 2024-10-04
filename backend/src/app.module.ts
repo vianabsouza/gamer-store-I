2d5b96675e5501b192112103b12bfebff5cc1dd0
Barbara Viana <vianabsouza@gmail.com>
2024-10-04 14:51:35 -0300

    feat: create 'produto' folder with module and controller, and update app.controller.ts and app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ProdutoModule } from './produto/produto.module';
 @Module({
-  imports: [],
+  imports: [ProdutoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
